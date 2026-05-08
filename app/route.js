2dfddf3d38447926a85eb7157817b0c84db3fb4a
ahmadlatifdev <privategate777@gmail.com>
2026-05-08 03:43:57 -0400

    remove route.js to allow static index

--- app/route.js
+++
@@ -1,5 +0,0 @@
-export async function GET() {
-  const fs = await import('fs/promises');
-  const html = await fs.readFile(process.cwd() + '/public/index.html', 'utf-8');
-  return new Response(html, { headers: { 'Content-Type': 'text/html' } });
-}
