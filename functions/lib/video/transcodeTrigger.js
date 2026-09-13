20ad0d180e54fdaf586aa31e446a86b954d6d019
ahmadlatifdev <privategate777@gmail.com>
2026-09-13 06:37:40 -0400

    fix(video): specify resumora-videos bucket in trigger

--- functions/lib/video/transcodeTrigger.js
+++ functions/lib/video/transcodeTrigger.js
@@ -4,10 +4,11 @@ const { TranscoderServiceClient } = require('@google-cloud/video-transcoder');
 const transcoder = new TranscoderServiceClient();
 const PROJECT_ID = process.env.GCLOUD_PROJECT || 'resumora-live';
 const LOCATION = 'us-central1';
+const BUCKET = 'resumora-videos';
 const TEMPLATE_ID = 'interview-hls-template';
 
 exports.autoTranscodeVideo = onObjectFinalized(
-  { region: 'us-central1', memory: '256MiB', timeoutSeconds: 60 },
+  { region: 'us-central1', bucket: BUCKET, memory: '256MiB', timeoutSeconds: 60 },
   async (event) => {
     const filePath = event.data.name;
     const bucket = event.data.bucket;
@@ -22,8 +23,6 @@ exports.autoTranscodeVideo = onObjectFinalized(
       outputUri: `gs://${bucket}/output/${outputPrefix}/`,
       templateId: `projects/${PROJECT_ID}/locations/${LOCATION}/jobTemplates/${TEMPLATE_ID}`,
     };
-
-    console.log('Starting transcoding job for ' + filePath);
 
     const [operation] = await transcoder.createJob({
       parent: `projects/${PROJECT_ID}/locations/${LOCATION}`,
